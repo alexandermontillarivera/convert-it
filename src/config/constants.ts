3b0e1a2964917f74e8b42a45a2f37f9b4b489478
Alexander Montilla Rivera <alexandermontillarivera@gmail.com>
2025-09-21 03:55:21 -0400

    feat: eliminar constantes de sistema de conversión no utilizadas

--- src/config/constants.ts
+++
@@ -1,8 +0,0 @@
-import { ConversionSystemBase } from "@/enums/conversion-system"
-
-export const REGEX_CONVERSION_SYSTEM: Record<ConversionSystemBase, RegExp> = {
-	[ConversionSystemBase.BINARY]: /^[0-1]+$/,
-	[ConversionSystemBase.DECIMAL]: /^\d+$/,
-	[ConversionSystemBase.OCTAL]: /^[0-7]+$/,
-	[ConversionSystemBase.HEXADECIMAL]: /^[0-9A-Fa-f]+$/,
-}
